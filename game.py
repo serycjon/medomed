9435c0269db1e090b870df974b254a57357a4322
serycjon <jonas.serych@gmail.com>
2018-05-11 23:19:39 +0200

    map, collisions

--- game.py
+++ game.py
@@ -1,38 +1,89 @@
-import pygame
+import pygame as pg
+import os
+import sys
 
-WIDTH = 800
-HEIGHT = 600
+from settings import *
+from sprites import *
 
-FPS = 30
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mixer.init()  # for sound
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100)
+        self.load_data()
 
-# Colors (R, G, B)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+    def load_data(self):
+        game_folder = os.path.dirname(__file__)
+        assets_folder = os.path.join(game_folder, 'assets')
 
-# initialize pygame and create window
-pygame.init()
-pygame.mixer.init()  # for sound
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Medomed")
-clock = pygame.time.Clock()
+        map_file = os.path.join(assets_folder, 'map.txt')
 
-# Game Loop
-running = True
-while running:
-    # keep running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
-    # Update
-    screen.fill(BLACK)
-    # Render (draw)
-    pygame.display.flip()
+        self.map_data = []
+        with open(map_file, 'r') as fin:
+            for line in fin:
+                self.map_data.append(line)
 
-pygame.quit()
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+
+        for r, tiles in enumerate(self.map_data):
+            for c, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, c, r)
+                if tile == 'P':
+                    self.player = Player(self, c, r)
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit(0)
+
+    def events(self):
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+        
+
+if __name__ == '__main__':
+    game = Game()
+    game.new()
+    game.run()
 
