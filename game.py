55a7bd617b5273382bdc8a4106928af434663958
serycjon <jonas.serych@gmail.com>
2018-05-15 00:10:28 +0200

    add: inventory visualization

--- game.py
+++ game.py
@@ -28,6 +28,7 @@ class Game:
         self.load_data()
         self.command_queue = Queue()
         self.ready_for_command = True
+        self.draw_inventory = False
 
     def load_img(self, path, size=None):
         img = pg.image.load(path).convert_alpha()
@@ -90,11 +91,17 @@ class Game:
         self.draw_debug = False
         self.ready_for_command = True
 
-    def draw_text(self, text, size, color, x, y):
+    def draw_text(self, text, size, color, x, y, align='midtop'):
         font = pg.font.Font(self.font_name, size)
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
-        text_rect.midtop = (x, y)
+        if align == 'midtop':
+            text_rect.midtop = (x, y)
+        elif align == 'topleft':
+            text_rect.topleft = (x, y)
+        else:
+            text_rect.bottomleft = (x, y)
+
         self.screen.blit(text_surface, text_rect)
 
     def menu(self):
@@ -142,6 +149,9 @@ class Game:
                     self.quit()
                 if event.key == pg.K_d:
                     self.draw_debug = not self.draw_debug
+                if event.key == pg.K_i:
+                    self.draw_inventory = not self.draw_inventory
+
         commands = {'forward': self.player.go_forward,
                     'turn': self.player.turn,
                     'status': self.status,
@@ -208,6 +218,44 @@ class Game:
             for wall in self.walls:
                 pg.draw.rect(self.screen, WHITE,
                              self.camera.apply_rect(wall.rect), 1)
+
+        # draw inventory
+        if self.draw_inventory:
+            inv_height = TILESIZE
+            inv_width = TILESIZE
+            inv_in_margin = 5
+            inv_out_margin = TILESIZE
+
+            inv_len = self.player.inventory_size
+            inv_H = inv_height
+            inv_W = inv_len * inv_width + (inv_len - 1) * inv_in_margin
+            inv_TL = (WIDTH - inv_W - inv_out_margin,
+                      HEIGHT - inv_out_margin - inv_H)
+
+            self.draw_text("INVENTORY", TILESIZE // 2, WHITE,
+                           inv_TL[0], inv_TL[1],
+                           align='bottomleft')
+            inv_surf = pg.Surface((inv_W, inv_H))
+            inv_surf.set_colorkey(BLACK)
+            # inv_surf.fill(DARKGREY)
+
+            for inv_place in range(inv_len):
+                rect = pg.Rect(inv_place * (inv_width + inv_in_margin),
+                               0,
+                               inv_width,
+                               inv_height)
+                lw = 1
+                if inv_place < len(self.player.inventory):
+                    # pg.draw.rect(inv_surf,
+                    #              LIGHTGREY,
+                    #              rect, 0)
+                    inv_surf.blit(self.item_images[self.player.inventory[inv_place]],
+                                  rect)
+
+                pg.draw.rect(inv_surf,
+                             WHITE,
+                             rect, 1)
+            self.screen.blit(inv_surf, inv_TL)
             
         pg.display.flip()
 
